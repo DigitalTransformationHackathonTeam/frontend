6e58190133fe5267302b2c11cffa2da76ebd38ad
Mikhail Lobanov <mp.lobanov@yandex.ru>
2020-10-31 19:02:13 +0300

    Resolving conflicts

--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -3,11 +3,15 @@ import './app.css';
 import MapView from "../../components/map";
 import CategorySelector from "../../components/category_selector";
 import Settings from "../../components/settings";
+import Info from "../../components/info";
 
 const App = () => (
     <div className="outer_wrap">
         <div className="cat_select">
             <CategorySelector/>
+        </div>
+        <div className="info-polygon">
+            <Info/>
         </div>
         <div className="settings_area">
             <Settings/>
@@ -16,7 +20,6 @@ const App = () => (
             <MapView/>
         </div>
     </div>
-
 );
 
 export default App;
