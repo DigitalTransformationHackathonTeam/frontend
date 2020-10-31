8ecdccc43a39435862a511fb464b2713b294a911
MarkTiukov <tyukov.mark@gmail.com>
2020-10-31 18:58:23 +0300

    Example of info card

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -4,11 +4,33 @@ import "./info.css"
 
 
 export default class Info extends React.Component {
+
+    state = {
+        is_filled: false,
+    };
+
+
     render() {
-        return (
-            <Card className="info-card">
-                <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>
-            </Card>
-        )
+        if (this.state.is_filled) {
+            return (
+                <Card className="info-card">
+                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>
+                </Card>
+            )
+        } else {
+            return (
+                <Card className="info-card">
+                    <h1>Точка №Х</h1>
+                    <p>
+                        <b>Скоринг:</b> 6/10
+                    </p>
+                    <p><h3>Почему перспективно</h3>
+                    Строится новый ЖК. Рядом появится школа и детский сад. Население - приемущественно женщины с выским доходом.
+                    </p>
+                    <p></p>
+                </Card>
+            )
+        }
+
     }
 }
--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './app.css';
 import MapView from "../../components/map";
 import CategorySelector from "../../components/category_selector";
+import Settings from "../../components/settings";
 import Info from "../../components/info";
 
 const App = () => (
@@ -11,6 +12,9 @@ const App = () => (
         </div>
         <div className="info-polygon">
             <Info/>
+        </div>
+        <div className="settings-area">
+            <Settings/>
         </div>
         <div className="map-container">
             <MapView/>
