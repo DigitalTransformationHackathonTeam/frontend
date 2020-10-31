3632ce75755d5043867ffaadf8044310f481f300
MarkTiukov <tyukov.mark@gmail.com>
2020-10-31 16:44:25 +0300

    Removed scale buttons

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -15,7 +15,7 @@ export default class MapView extends React.Component {
     }
 
     componentDidMount() {
-        fetch('http://0.0.0.0:8091/api/location_optimizer/find_best_district')
+        fetch('http://192.168.0.115:8000/api/location_optimizer/find_best_district/Moscow/pharmacy')
             .then(res => res.json())
             .then(
                 (result) => {
@@ -46,7 +46,7 @@ export default class MapView extends React.Component {
             return <div>Loading...</div>;
         } else {
             return (
-                <Map center={[55.75229, 37.61593]} zoom={13} className='map-wrapper'>
+                <Map center={[55.75229, 37.61593]} zoom={13} className='map-wrapper' zoomControl={false}>
                     <TileLayer
                         attribution='&copy; <a href="copyright">Openstreetmap</a>'
                         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
