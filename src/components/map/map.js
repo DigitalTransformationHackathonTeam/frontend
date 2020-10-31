ebd339429609bbee7a9b293621fc8d062fbdae87
MarkTiukov <tyukov.mark@gmail.com>
2020-10-31 11:41:09 +0300

    Examples

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -6,64 +6,98 @@ import './map.css';
 
 export default class MapView extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            polygon: {}
+        };
+    }
+
+    // error = null;
+    // isLoaded = false;
+    // polygon = {};
+
+    componentDidMount() {
+        fetch('http://0.0.0.0:8091/api/location_optimizer/find_best_district')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        polygon: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
-        const geoPolygon = {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "properties": {},
-                    "geometry": {
-                        "type": "Polygon",
-                        "coordinates": [
-                            [
+        const {error, isLoaded, polygon} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            const geoPolygon = {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                        "type": "Feature",
+                        "properties": {},
+                        "geometry": {
+                            "type": "Polygon",
+                            "coordinates": [
                                 [
-                                    37.624053955078125,
-                                    55.767303495700936
-                                ],
-                                [
-                                    37.58697509765625,
-                                    55.75880449639896
-                                ],
-                                [
-                                    37.591094970703125,
-                                    55.741027874718164
-                                ],
-                                [
-                                    37.61787414550781,
-                                    55.73793546140932
-                                ],
-                                [
-                                    37.6556396484375,
-                                    55.74180093975863
-                                ],
-                                [
-                                    37.64739990234375,
-                                    55.76382685625667
-                                ],
-                                [
-                                    37.624053955078125,
-                                    55.767303495700936
+                                    [
+                                        37.624053955078125,
+                                        55.767303495700936
+                                    ],
+                                    [
+                                        37.58697509765625,
+                                        55.75880449639896
+                                    ],
+                                    [
+                                        37.591094970703125,
+                                        55.741027874718164
+                                    ],
+                                    [
+                                        37.61787414550781,
+                                        55.73793546140932
+                                    ],
+                                    [
+                                        37.6556396484375,
+                                        55.74180093975863
+                                    ],
+                                    [
+                                        37.64739990234375,
+                                        55.76382685625667
+                                    ],
+                                    [
+                                        37.624053955078125,
+                                        55.767303495700936
+                                    ]
                                 ]
                             ]
-                        ]
+                        }
                     }
-                }
-            ]
-        };
-        const points = [[55.74, 37.61], [55.76, 37.61], [55.76, 37.63], [55.74, 37.63]]
-        return (
-            <Map center={[55.75229, 37.61593]} zoom={13} className='map-wrapper'>
-                <TileLayer
-                    attribution='&copy; <a href="copyright">Openstreetmap</a>'
-                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                />
-                <Polygon
-                    positions={points}
-                    color={'#e90c0c'}
-                />
-                <GeoJSON data={geoPolygon} color={"#7608e3"}/> // мб нужен свой ключ: key={hash(geoPolygon)}
-            </Map>
-        );
+                ]
+            };
+            return (
+                <Map center={[55.75229, 37.61593]} zoom={13} className='map-wrapper'>
+                    <TileLayer
+                        attribution='&copy; <a href="copyright">Openstreetmap</a>'
+                        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    />
+                    <GeoJSON data={this.state.polygon} color={"#35ab03"} weight={2} fillOpacity={0.5}/> // мб нужен свой ключ: key={hash(geoPolygon)}
+                </Map>
+            );
+        }
     }
 }
